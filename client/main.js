8e3ddc5aadab97168bb0af517a08f65fb325524a
nick <nick.hax@gmail.com>
2016-05-28 15:28:35 -0400

    fleshing out todo, added iron router

--- client/main.js
+++ client/main.js
@@ -33,5 +33,60 @@ Template.todoItem.events({
         if(confirm){
             Todos.remove({_id: documentId});
         }
+    },
+
+    'keyup [name=todoItem]': function(event){
+        if(event.which == 13 || event.which == 27){
+            $(event.target).blur();
+        } else {
+            var documentId = this._id;
+            var todoItem = $(event.target).val();
+            Todos.update({_id: documentId}, {$set: {name: todoItem}});
+        }
+    },
+
+    'change [type=checkbox]': function() {
+        var documentId = this._id;
+        var isCompleted = this.completed;
+        if(isCompleted){
+            Todos.update({_id: documentId}, {$set: {completed: false}});
+        } else {
+            Todos.update({_id: documentId}, {$set: {completed: true}});
+        }
     }
+
+});
+
+Template.todoItem.helpers({
+    'checked': function() {
+        var isCompleted = this.completed;
+        if(isCompleted){
+            return "checked";
+        } else {
+            return "";
+        }
+    }
+});
+
+Template.todosCount.helpers({
+    'totalTodos': function(){
+        return Todos.find().count();
+    },
+
+    'completedTodos': function(){
+        return Todos.find({completed: true}).count();
+    }
+});
+
+Router.configure({
+    layoutTemplate: 'main'
+});
+
+Router.route('/register');
+
+Router.route('/login');
+
+Router.route('/', {
+    name: 'home',
+    template: 'home'
 });
