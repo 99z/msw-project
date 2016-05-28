b2b72c0a1ebc4efb693b6e1dc6a38246df3a87e7
nick <nick.hax@gmail.com>
2016-05-28 15:53:29 -0400

    creating users

--- client/main.js
+++ client/main.js
@@ -78,6 +78,48 @@ Template.todosCount.helpers({
     }
 });
 
+Template.register.events({
+    'submit form': function(event) {
+        event.preventDefault();
+        var username = $('[name=username]').val();
+        var password = $('[name=password]').val();
+        Accounts.createUser({
+            username: username,
+            password: password
+        }, function(error) {
+            if(error) {
+                alert(error.reason);
+            } else {
+                Router.go('home');
+            }
+        });
+        Router.go('home');
+    }
+});
+
+Template.login.events({
+    'submit form': function(event) {
+        event.preventDefault();
+        var username = $('[name=username]').val();
+        var password = $('[name=password]').val();
+        Meteor.loginWithPassword(username, password, function(error) {
+            if(error) {
+                alert(error.reason);
+            } else {
+                Router.go('home');
+            }
+        });
+    }
+});
+
+Template.navigation.events({
+    'click .logout': function(event) {
+        event.preventDefault();
+        Meteor.logout();
+        Router.go('login');
+    }
+});
+
 Router.configure({
     layoutTemplate: 'main'
 });
