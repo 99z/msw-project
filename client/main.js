6c01ae3bb7464d23c0e48249bd78a88d3a42e322
nick <nick.hax@gmail.com>
2016-05-29 20:58:47 -0400

    clean

--- client/main.js
+++ client/main.js
@@ -1,11 +1,7 @@
-if(Meteor.isClient){
-    // client code goes here
-}
-
-if(Meteor.isServer){
-    // server code goes here
-}
-
+/*
+When a user submits the registration form, create + add them to DB and log in.
+Sends the newly registered and logged in user to the home page.
+ */
 Template.register.events({
     'submit form': function(event) {
         event.preventDefault();
@@ -14,24 +10,22 @@ Template.register.events({
         Accounts.createUser({
             username: username,
             password: password
-        }, function(error) {
-            if(error) {
-                alert(error.reason);
-            } else {
-                Router.go('home');
-            }
         });
         Router.go('home');
     }
 });
 
+/*
+When a user submits the login form, logs them in to their account.
+Prints an error reason to the console if an error occurs during login.
+ */
 Template.login.events({
     'submit form': function(event) {
         event.preventDefault();
         var username = $('[name=username]').val();
         var password = $('[name=password]').val();
         Meteor.loginWithPassword(username, password, function(error) {
-            if(error) {
+            if (error) {
                 alert(error.reason);
             } else {
                 Router.go('home');
@@ -40,6 +34,13 @@ Template.login.events({
     }
 });
 
+/*
+Allows the user to submit the new post form for a particular page they operate.
+Calls the publishPost method on the server, which uses FBGraph API to make
+the post.
+Logs an error to the console if something went wrong.
+Finally, replaces placeholder text with a confirmation message.
+ */
 Template.pageView.events({
     'submit .new-post': function(event) {
         event.preventDefault();
@@ -47,14 +48,16 @@ Template.pageView.events({
         Meteor.call("publishPost", this.id, content, function(error, accessToken) {
             if (error) {
                 console.log(error.reason);
-            } else {
-                console.log(accessToken);
             }
         });
         $("#postArea").attr("placeholder", "Posted! Got more to say?").val("").focus().blur();
     }
 });
 
+/*
+On template instantiation, call getPages method on server to request pages
+the currently logged in user has permission to post as.
+ */
 Template.pages.created = function() {
     this.pages = new ReactiveVar([]);
 
@@ -68,10 +71,17 @@ Template.pages.created = function() {
     });
 }
 
+/*
+With the list of pages returned upon template instantiation, insert any pages
+that are not already in the database to the Pages DB.
+Return a list of the pages the user has access to post as for the DOM.
+ */
 Template.pages.helpers({
     'page': function() {
         Template.instance().pages.get().forEach(function(item) {
-            if (!Pages.findOne({name: item.name})) {
+            if (!Pages.findOne({
+                    name: item.name
+                })) {
                 Pages.insert({
                     name: item.name,
                     id: item.id
@@ -82,6 +92,10 @@ Template.pages.helpers({
     }
 });
 
+/*
+Logs a user out upon clicking the logout link in the navbar.
+Opens a Facebook OAuth login prompt upon clicking the FB Login link in navbar.
+ */
 Template.navigation.events({
     'click .logout': function(event) {
         event.preventDefault();
@@ -100,6 +114,9 @@ Template.navigation.events({
     }
 });
 
+/*
+Below is basic router configuration.
+ */
 Router.configure({
     layoutTemplate: 'main'
 });
@@ -119,6 +136,8 @@ Router.route('/page/:id', {
     template: 'pageView',
     data: function() {
         var currentPage = String(this.params.id);
-        return Pages.findOne({id: this.params.id});
+        return Pages.findOne({
+            id: this.params.id
+        });
     }
 });
--- server/main.js
+++ server/main.js
@@ -1,5 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 
+/*
+Required configuration to hook the application into my FB testing app.
+ */
 ServiceConfiguration.configurations.upsert(
     {service: "facebook"},
     {
@@ -15,6 +18,11 @@ ServiceConfiguration.configurations.upsert(
 Meteor.startup(() => {
     Meteor.methods({
 
+        /*
+        Saves the currently logged in user's access token for the FB graph API.
+        Makes a synchronous call to the FB graph API to return a list of pages
+        owned by the currently logged in user.
+         */
         "getPages": function() {
             FBGraph.setAccessToken(Meteor.user().services.facebook.accessToken);
             var facebookUserId = Meteor.user().services.facebook.id;
@@ -24,19 +32,20 @@ Meteor.startup(() => {
             return result.data;
         },
 
+        /*
+        Publishes a post with passed content to a page whose ID is also passed.
+        Gets the access token for the page we want to post to, then posts.
+        Logs an error to the console if there is one.
+         */
         "publishPost": function(pageID, postcontent) {
             var token_request = { fields: "access_token" };
             var getATSync = Meteor.wrapAsync(FBGraph.get);
             var pageAT = getATSync(pageID, token_request);
 
-            console.log(pageAT.access_token);
-
             var args = { access_token: pageAT.access_token, message: postcontent};
             FBGraph.post(pageID + "/feed", args, function(err, res) {
                 if (err) {
                     console.log(err);
-                } else {
-                    console.log(res);
                 }
             });
         }
